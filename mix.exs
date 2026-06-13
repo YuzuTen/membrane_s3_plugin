ca1809de63bf72298323ba646bb6dd4cc39ea6e9
Jason Truesdell <jason@yuzuten.com>
2026-06-13 18:54:20 +0900

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.S3.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.1.2"
+  @version "0.2.0"
   @github_url "https://github.com/YuzuTen/membrane_s3_plugin"
 
   def project do
